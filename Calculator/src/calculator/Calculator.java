f08844dcf090e41e447283312c242f8fff2fea92
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2023-06-05 00:44:38 +0600

    initialization

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -16,11 +16,25 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener {
 
-    /**
-     * @param args the command line arguments
-     */
+    // all the initialization
+    JFrame frame; //bg
+    JTextField txtfield; //display 
+    JButton[] num_buttons = new JButton[10]; //0,1,2,3,4,5,6,7,8,9
+    JButton[] func_buttons = new JButton[8]; //
+    JButton add,sub,multi,div;
+    JButton dec,equ,del,clr;
+    JPanel panel;
+    
+    
+    Calculator(){
+        
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+         Calculator calc = new Calculator();
+         
     }
 
     @Override
